df39a412556e03438d4581e2f618d8a2db855c29
Matthias Andreas Benkard <code@mail.matthias.benkard.de>
2013-08-31 12:29:23 +0200

    [eval2] Rewrite %TAGBODY.

--- src/code/eval2-transformers.lisp
+++ src/code/eval2-transformers.lisp
@@ -28,6 +28,9 @@
 (define-symbol-macro go-tag-function-mapping nil)
 (define-symbol-macro go-tag-catch-tag-mapping nil)
 
+(defmacro %loop (&body forms)
+  `(loop ,@forms))
+
 (defmacro %go (tag &environment env)
   `(throw ',(cdr (assoc tag (macroexpand 'go-tag-catch-tag-mapping env)
                         :test 'eq))
@@ -35,64 +38,34 @@
                             :test 'eq)))))
 
 (defmacro %tagbody (&body body &environment env)
-  (let* (labels-and-catch-tags
-         labels-and-functions
-         (catch-tag (gensym "TAGBODY-CATCH-TAG"))
+  (let* ((catch-tag (gensym "TAGBODY-CATCH-TAG"))
          (block-name (gensym "TAGBODY-BLOCK-NAME"))
          (return-value-sym (gensym "TAGBODY-RETURN-VALUE"))
          (end-marker (gensym "TAGBODY-END"))
+         (labels-and-bodies (parse-tagbody-tags-and-bodies body))
+         (labels-and-catch-tags
+           (mapcar (lambda (l&b) (cons (first l&b) catch-tag))
+                   labels-and-bodies))
+         (labels-and-functions
+           (mapcar (lambda (l&b) (cons (first l&b) (gensym "TAGBODY-SECTION")))
+                   labels-and-bodies))
          (sections
-          (mapcon (let (current-label
-                        accumulated-clauses
-                        current-function)
-                    (lambda (clause-and-rest)
-                      (let ((clause (car clause-and-rest))
-                            (rest (cdr clause-and-rest)))
-                        (cond
-                          ((atom clause)
-                           (when current-function
-                             (push (cons current-label current-function)
-                                    labels-and-functions)
-                             (push (cons current-label catch-tag)
-                                    labels-and-catch-tags))
-                           (let ((old-function current-function))
-                             (setq current-label clause
-                                   current-function (gensym "TAGBODY-SECTION"))
-                             (prog1
-                               `((,old-function ()
-                                   ;; Yes, we generate this even when
-                                   ;; OLD-FUNCTION is NIL.  In this
-                                   ;; case, though, the LABELS form
-                                   ;; never sees the definition.  Grep
-                                   ;; for (cddr (first sections)) below
-                                   ;; in order to see how we make use of
-                                   ;; the generated lambda form instead.
-                                   ,@(nreverse accumulated-clauses)
-                                   #',current-function)
-                                 ,@(when (endp rest)
-                                     `((,current-function ()
-                                                          ',end-marker))))
-                               (setq accumulated-clauses nil))))
-                          (t (push clause accumulated-clauses)
-                             (if (endp rest)
-                                 (progn
-                                   (when current-function
-                                     (push (cons current-label current-function)
-                                           labels-and-functions)
-                                     (push (cons current-label catch-tag)
-                                           labels-and-catch-tags))
-                                   `((,current-function ()
-                                        ,@(nreverse accumulated-clauses)
-                                        ',end-marker)))
-                                 nil))))))
-                   body)))
+           (loop with functions = (mapcar #'cdr labels-and-functions)
+                 for (label . body) in labels-and-bodies
+                 for next-throw in (append (mapcar #'(lambda (fn) `(function ,fn))
+                                                   (rest functions))
+                                           (list `(quote ,end-marker)))
+                 for function-name in (mapcar #'cdr labels-and-functions)
+                 collect `(,function-name ()
+                            ,@body
+                            ,next-throw))))
     `(symbol-macrolet ((go-tag-catch-tag-mapping
                          ,(append labels-and-catch-tags
                                   (macroexpand 'go-tag-catch-tag-mapping env)))
                        (go-tag-function-mapping
                          ,(append labels-and-functions
                                   (macroexpand 'go-tag-function-mapping env))))
-       (labels (,@(rest sections))
+       (labels (,@sections)
          (%block ,block-name
            (let (,return-value-sym)
              (%loop
@@ -100,6 +73,6 @@
                      (catch ',catch-tag
                        (if ,return-value-sym
                            (funcall ,return-value-sym)
-                           (progn ,@(cddr (first sections))))))
+                           (,(cdar labels-and-functions)))))
                (when (eq ,return-value-sym ',end-marker)
                  (%return-from ,block-name nil)))))))))
