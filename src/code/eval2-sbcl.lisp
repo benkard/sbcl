6eb4b356afe128078cce51cf170c9408ac173735
Matthias Andreas Benkard <code@mail.matthias.benkard.de>
2013-09-02 16:56:03 +0200

    [eval2] Store source location information about a closure within the closure itself.

--- src/code/eval2-sbcl.lisp
+++ src/code/eval2-sbcl.lisp
@@ -3,7 +3,10 @@
 (declaim (optimize (debug 2) (space 2) (speed 2) (safety 0) (compilation-speed 0)
                    (sb!c::store-closure-debug-pointer 3)))
 
-(defvar *source-paths&locations* (make-hash-table :weakness :key :test #'eq))
+(defvar *dump-source-p* nil)
+(defvar *source-paths&locations* (make-hash-table :weakness :key
+                                                  :test #'eq
+                                                  :synchronized t))
 (defparameter *debug-interpreter* nil)
 
 (defun interpreted-function-source-location (function)
@@ -13,9 +16,19 @@
 (defun source-location (closure)
   (cdr (source-path&location closure)))
 (defun source-path&location (closure)
-  (gethash closure *source-paths&locations*))
+  (sb-ext:with-locked-hash-table (*source-paths&locations*)
+    (gethash/set-default closure *source-paths&locations*
+      (let ((function-name
+              (and (functionp closure)
+                   (sb!kernel:%fun-name
+                    (sb!impl::%fun-fun closure)))))
+        (if (eq function-name 'interpreted-function)
+            (let ((*dump-source-p* t))
+              (funcall (the function closure)))
+            nil)))))
 (defun (setf source-path&location) (val closure)
-  (setf (gethash closure *source-paths&locations*) val))
+  (sb-ext:with-locked-hash-table (*source-paths&locations*)
+    (setf (gethash closure *source-paths&locations*) val)))
 
 (defun annotate-lambda-with-source (closure &optional
                                             (current-path
@@ -33,10 +46,6 @@
     ;; but not relevant for the form we are processing).
     (setf (source-path&location closure) (cons current-path source-location)))
   closure)
-(defun annotate-interpreted-lambda-with-source (closure current-path source-location)
-  (declare (ignorable current-path source-location))
-  (annotate-lambda-with-source closure current-path source-location)
-  closure)
 (defmacro eval-lambda (lambda-list (&optional kind current-path source-loc) &body body)
   `(annotate-lambda-with-source
     (sb!int:named-lambda ,(if kind
@@ -48,12 +57,11 @@
     ,source-loc))
 (defmacro interpreted-lambda ((name current-path source-loc) lambda-list &body body)
   (declare (ignore name))
-  `(annotate-interpreted-lambda-with-source
-    (sb!int:named-lambda interpreted-function ,lambda-list
-      (declare (optimize sb!c::store-closure-debug-pointer))
-      ,@body)
-    ,current-path
-    ,source-loc))
+  `(sb!int:named-lambda interpreted-function ,lambda-list
+     (declare (optimize sb!c::store-closure-debug-pointer))
+     (if *dump-source-p*
+         (cons ,current-path ,source-loc)
+         (progn ,@body))))
 
 (defun self-evaluating-p (form)
   (sb!int:self-evaluating-p form))
